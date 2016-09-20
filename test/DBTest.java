2d18648b2bfb9c9d705adafdd5b8df520a300ec8
fantay <laurentcoll@hotmail.com>
2016-09-20 16:24:00 +0200

    ajout de la page de test de la base

---
+++ test/DBTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.persistence.Persistence;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Laurent-LIM
+ */
+public class DBTest {
+
+    @Test
+    public void test() {
+
+        Persistence.createEntityManagerFactory("PU");
+    }
+}
